4ed6002d03e1707402dfa4169e6308349ecbd925
Ján Mikula <mikulajan180@gmail.com>
2021-12-06 12:55:39 +0100

    Renamed DebugSmsService.cs to FileSystemSmsService.cs due recommendation.

---
+++ Origam-sms/FileSystemSmsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Origam.Sms;
+
+namespace Origam_sms
+{
+    public class FileSystemSmsService : ISmsService
+    {
+        public int SendSms(string from, string to, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
