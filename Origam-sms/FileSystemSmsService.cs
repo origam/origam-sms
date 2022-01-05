bf46a05b34b54b826eb9b0320c1b7f2d0728ee10
Ján Mikula <mikulajan180@gmail.com>
2022-01-05 15:02:46 +0100

    + FileSystemSmsService.cs for easy Service Testing. Will output fake sending with parameters to console.

---
+++ Origam-sms/FileSystemSmsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Origam.Sms
+{
+    public class FileSystemSmsService : ISmsService
+    {
+        public int SendSms(string from, string to, string body)
+        {
+            Console.WriteLine($"Sending sms from {from} to {to}, with text {body}");
+            return 1;
+        }
+    }
+}
